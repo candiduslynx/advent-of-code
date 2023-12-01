c096211bc866da3534d7fe590134f6245bf43878
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-12-01 13:53:08 +0200

    2023.1 (#1)
    * 1.1
    
    * 1.2

---
+++ 2023/1/src/main.rs
@@ -0,0 +1,8 @@
+mod part1;
+mod part2;
+
+fn main() {
+    println!("The result for part1 is {}", part1::solve());
+    println!("The result for part2 is {}", part2::solve());
+}
+
---
+++ 2023/1/src/part1.rs
@@ -0,0 +1,17 @@
+use std::fs::read;
+use std::io::BufRead;
+
+pub(crate) fn solve() ->u32{
+    return  read("./input.txt").unwrap().lines().
+        map(|s| extract_num(s.unwrap())).
+        fold(0, |sum:u32, x| sum + (x as u32));
+}
+
+fn extract_num(string: String) -> u8 {
+    if string.is_empty() {
+        return 0;
+    }
+
+    let digits: Vec<u8> = string.split("").filter_map(|s| s.parse().ok()).collect();
+    return digits.first().unwrap() * 10 + digits.last().unwrap();
+}
