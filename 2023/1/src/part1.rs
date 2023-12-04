ac6198f3756804cc266b7c1c2ccda98fcf51727b
candiduslynx <candiduslynx@gmail.com>
2023-12-04 11:56:14 +0200

    use path

--- 2023/1/src/part1.rs
+++ 2023/1/src/part1.rs
@@ -1,9 +1,10 @@
 use std::fs::read;
 use std::io::BufRead;
 
-pub(crate) fn solve() -> u32 {
-    read("./input.txt").unwrap().lines().
+pub(crate) fn solve(path: &str) -> u32 {
+    read(path).unwrap().lines().
         map(|s| s.unwrap().chars().filter_map(|c|c.to_digit(10)).collect::<Vec<u32>>()). // digits from the line
+        filter(|digits| !digits.is_empty()).
         map(|digits|digits.first().unwrap() * 10 + digits.last().unwrap()). // take first & last
         sum()
 }
--- 2023/1/src/part2.rs
+++ 2023/1/src/part2.rs
@@ -1,8 +1,8 @@
 use std::fs::read;
 use std::io::BufRead;
 
-pub(crate) fn solve() -> u32 {
-    read("./input.txt").unwrap().lines().
+pub(crate) fn solve(path: &str) -> u32 {
+    read(path).unwrap().lines().
         map(|s| s.unwrap()).
         map(|s| s.replace("zero", "0o")). // _o_ne
         map(|s| s.replace("one", "o1e")). // zer_o_, tw_o_, _e_ight
@@ -15,6 +15,7 @@ pub(crate) fn solve() -> u32 {
         map(|s| s.replace("eight", "e8t")). // on_e_, _t_wo, _t_hre_e_, fiv_e_
         map(|s| s.replace("nine", "n9e")). // seve_n_, _e_ight
         map(|s| s.chars().filter_map(|c|c.to_digit(10)).collect::<Vec<u32>>()). // digits from the line
+        filter(|digits| !digits.is_empty()).
         map(|digits|digits.first().unwrap() * 10 + digits.last().unwrap()). // take first & last
         sum()
 }
