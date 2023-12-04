c3fd3cbc3a47f8675d9c00593acf72ba70d88294
candiduslynx <candiduslynx@gmail.com>
2023-12-04 11:26:39 +0200

    1.1

--- 2023/1/src/part1.rs
+++ 2023/1/src/part1.rs
@@ -3,15 +3,7 @@ use std::io::BufRead;
 
 pub(crate) fn solve() -> u32 {
     read("./input.txt").unwrap().lines().
-        map(|s| extract_num(s.unwrap())).
-        fold(0, |sum: u32, x| sum + (x as u32))
-}
-
-fn extract_num(string: String) -> u8 {
-    if string.is_empty() {
-        return 0;
-    }
-
-    let digits: Vec<u8> = string.split("").filter_map(|s| s.parse().ok()).collect();
-    digits.first().unwrap() * 10 + digits.last().unwrap()
+        map(|s| s.unwrap().chars().filter_map(|c|c.to_digit(10)).collect::<Vec<u32>>()). // digits from the line
+        map(|digits|digits.first().unwrap() * 10 + digits.last().unwrap()). // take first & last
+        sum()
 }
