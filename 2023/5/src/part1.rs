1e8bd0b2c0e6470c778cf4078272176deab2552c
candiduslynx <candiduslynx@gmail.com>
2023-12-05 18:53:04 +0200

    reformat

--- 2023/5/src/part1.rs
+++ 2023/5/src/part1.rs
@@ -1,8 +1,9 @@
 use std::fs::read;
 use std::io::BufRead;
+
 use crate::almanac::Almanac;
 
 pub(crate) fn solve(path: &str) -> u64 {
     let almanac = Almanac::from_lines(read(path).unwrap().lines());
-    almanac.seeds.iter().map(|s|almanac.location_for_seed(s)).min().unwrap()
+    almanac.seeds.iter().map(|s| almanac.location_for_seed(s)).min().unwrap()
 }
