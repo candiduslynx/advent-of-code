8dfc3c68ccd3e0ea4f188031f7f34181bac48ce2
candiduslynx <candiduslynx@gmail.com>
2023-12-07 09:22:10 +0200

    shorten 2023.6

--- 2023/6/src/equation.rs
+++ 2023/6/src/equation.rs
@@ -8,33 +8,26 @@
 pub(crate) fn solutions(time: i64, distance: i64) -> u64 {
     let roots = solve_sq_eq(1.0, -(time as f64), distance as f64 + 1.0);
 
-    if roots.0.is_none() {
+    if roots.is_none() {
         return 0;
     }
-    let r0 = roots.0.unwrap();
+    let (r0, r1) = roots.unwrap();
 
-    if roots.1.is_none() {
-        // check that the only root is actually an integer
-        if r0.fract() == 0.0 { return 1; } else { return 0; }
-    }
-    let r1 = roots.1.unwrap();
+    let solutions = (r1.floor() as i32) - (r0.ceil() as i32) + 1;
 
-    let solutions = (r1.floor() as i32) - (r0.floor() as i32) + if r0.fract() == 0.0 || r1.fract() == 0.0 { 1 } else { 0 };
-    if solutions < 0 { 0 } else { solutions as u64 }
+    // if we have a single root that's not integer we'll get solutions=0 above
+    solutions as u64
 }
 
-fn solve_sq_eq(a: f64, b: f64, c: f64) -> (Option<f64>, Option<f64>) {
-    assert!(a != 0.0);
+fn solve_sq_eq(a: f64, b: f64, c: f64) -> Option<(f64, f64)> {
+    assert_ne!(a, 0.0);
 
     let d: f64 = b * b - 4.0 * a * c;
     if d < 0.0 {
-        return (None, None);
+        return None;
     }
 
     let x = -0.5 * b / a;
-    if d == 0.0 {
-        return (Some(x), None);
-    }
-    let d1 = 0.5 * d.sqrt() / a;
-    (Some(x - d1), Some(x + d1))
+    let ds = 0.5 * d.sqrt() / a;
+    Some((x-ds, x+ds))
 }
