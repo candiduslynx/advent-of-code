a1a47693a151fdea6d62d553fed3edbcbf6643f9
candiduslynx <candiduslynx@gmail.com>
2023-12-09 14:41:41 +0200

    refactor

--- 2023/9/src/part2.rs
+++ 2023/9/src/part2.rs
@@ -1,24 +1,21 @@
 use std::fs::read;
 use std::io::BufRead;
+
 use crate::binomial;
 
-// to find X = x_(n+1) we're solving the eq:
-// sum(i=0..=(n+1))( (-1.pow(n+1-i))*c(n+1,i)*x_i) = 0
-// => X = sum(i=0..=n)( (-1.pow(n-i))*c(n+1,i)*x_i)
-//
-// where c(n,k) = (n!)/((n-k)!*k!)
+// part 2 is to find x_(-1), which is the same as to solve part 1 for reversed sequence
 pub(crate) fn solve(path: &str) -> i64 {
-    let nums: Vec<Vec<i64>> = read(path).unwrap().lines()
+    let c = binomial::cnk(25);
+    read(path).unwrap().lines()
         .map(|s| s.unwrap()).filter(|s| !s.is_empty())
-        .map(|s| s.split_whitespace().filter_map(|s| s.parse::<i64>().ok()).collect())
-        .collect();
-
-    let max_len = nums.iter().map(|nums| nums.len()).max().unwrap();
-    let c = binomial::cnk(max_len + 1);
-
-    nums.iter().map(|nums| {
-        let n = nums.len();
-        let coef = |i| { if (n - i) & 1 == 0 { -1i64 } else { 1i64 } };
-        nums.iter().rev().enumerate().map(|(i, x)| coef(i) * c[n][i] * x).sum::<i64>()
-    }).sum()
+        .map(|s| {
+            let nums: Vec<i64> = s.split_whitespace().filter_map(|s| s.parse::<i64>().ok()).collect();
+            let n = nums.len();
+            // based on the len we want -1,1,... or 1,-1,... as coefficients
+            (if n & 1 == 0 { vec![-1i64, 1i64] } else { vec![1i64, -1i64] }).iter().cycle()
+                .zip(nums.iter().rev())
+                .enumerate()
+                .map(|(i, (k, x))| k * c[n][i] * x)
+                .sum::<i64>()
+        }).sum()
 }
