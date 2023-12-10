de8003bd677d8589ae1a2b4c08aa772255204ba2
candiduslynx <candiduslynx@gmail.com>
2023-12-10 20:00:29 +0200

    refactor 2023.01.2

--- 2023/01/src/part2.rs
+++ 2023/01/src/part2.rs
@@ -2,20 +2,46 @@ use std::fs::read;
 use std::io::BufRead;
 
 pub(crate) fn solve(path: &str) -> u32 {
-    read(path).unwrap().lines().
-        map(|s| s.unwrap()).
-        map(|s| s.replace("zero", "0o")). // _o_ne
-        map(|s| s.replace("one", "o1e")). // zer_o_, tw_o_, _e_ight
-        map(|s| s.replace("two", "t2o")). // _o_ne, eigh_t_
-        map(|s| s.replace("three", "t3e")). // _e_igh_t_
-        map(|s| s.replace("four", "4")).
-        map(|s| s.replace("five", "5e")). // _e_ight
-        map(|s| s.replace("six", "6")).
-        map(|s| s.replace("seven", "7n")). // _n_ine
-        map(|s| s.replace("eight", "e8t")). // on_e_, _t_wo, _t_hre_e_, fiv_e_
-        map(|s| s.replace("nine", "n9e")). // seve_n_, _e_ight
-        map(|s| s.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<u32>>()). // digits from the line
-        filter(|digits| !digits.is_empty()).
-        map(|digits| digits.first().unwrap() * 10 + digits.last().unwrap()). // take first & last
-        sum()
+    read(path)
+        .unwrap()
+        .lines()
+        .map(|s| s.unwrap())
+        .filter(|s| !s.is_empty())
+        .map(|s| digits(s.as_str()))
+        .map(|digits| digits.first().unwrap() * 10 + digits.last().unwrap())
+        .sum()
+}
+
+fn digits(s: &str) -> Vec<u32> {
+    s.char_indices()
+        .filter_map(|(idx, _)| digit(&s[idx..]))
+        .collect()
+}
+
+fn digit(s: &str) -> Option<u32> {
+    if s.is_empty() {
+        return None;
+    }
+
+    match s.chars().next() {
+        None => return None,
+        Some(c) => {
+            if c.is_ascii_digit() {
+                return c.to_digit(10);
+            }
+        }
+    }
+
+    [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .iter()
+    .enumerate()
+    .find_map(|(i, &pfx)| {
+        if s.starts_with(pfx) {
+            Some(i as u32)
+        } else {
+            None
+        }
+    })
 }
