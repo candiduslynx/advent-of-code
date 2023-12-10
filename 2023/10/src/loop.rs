7646475b7c1c3645746c255d3fc6e690ecb24de4
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-12-10 17:45:12 +0200

    refactor 2023.10 (#14)

--- 2023/10/src/loop.rs
+++ 2023/10/src/loop.rs
@@ -4,103 +4,89 @@ use lib::point::Point;
 
 pub(crate) fn get_loop(
     lines: &Vec<Vec<char>>,
-    start: &(usize, usize),
-    treat: char,
+    start: Point,
+    treat: &char,
 ) -> Option<HashSet<Point>> {
-    let start = Point {
-        x: start.0 as isize,
-        y: start.1 as isize,
-    };
     let mut pos = start;
     let mut pipe = treat;
     let mut prev: Option<Point> = None;
     let (max_x, max_y) = (lines.len(), lines[0].len());
     let mut result: HashSet<Point> = HashSet::new();
 
-    while pos != start || result.len() == 0 {
-        let next: Option<Point> = match pipe {
-            '|' => match prev {
-                None => Some(pos.below()),
-                Some(_) => Some(Point {
-                    x: (pos.x << 1) - prev.unwrap().x,
-                    y: pos.y,
-                }),
+    while prev != Some(start) {
+        let next = match prev {
+            Some(prev) => {
+                match pipe {
+                    '|' => Point {
+                        x: (pos.x << 1) - prev.x,
+                        y: pos.y,
+                    },
+                    '-' => Point {
+                        x: pos.x,
+                        y: (pos.y << 1) - prev.y,
+                    },
+                    'F' => Point {
+                        x: (pos.x << 1) - prev.x + 1, // below => prev.x = pos.x + 1, right => pos.x = prev.x
+                        y: (pos.y << 1) - prev.y + 1, // below => prev.y = pos.y, right => pos.y = prev.y + 1
+                    },
+                    '7' => Point {
+                        x: (pos.x << 1) - prev.x + 1, // below => prev.x = pos.x + 1, left => pos.x = prev.x
+                        y: (pos.y << 1) - prev.y - 1, // below => prev.y = pos.y, left => pos.y = prev.y - 1
+                    },
+                    'J' => Point {
+                        x: (pos.x << 1) - prev.x - 1, // above => prev.x = pos.x - 1, left => pos.x = prev.x
+                        y: (pos.y << 1) - prev.y - 1, // above => prev.y = pos.y, left => pos.y = prev.y - 1
+                    },
+                    'L' => Point {
+                        x: (pos.x << 1) - prev.x - 1, // above => prev.x = pos.x - 1, left => pos.x = prev.x
+                        y: (pos.y << 1) - prev.y + 1, // below => prev.y = pos.y, right => pos.y = prev.y + 1
+                    },
+                    _ => return None,
+                }
+            }
+            None => match pipe {
+                '|' | 'F' | '7' => pos.below(),
+                '-' | 'J' => pos.left(),
+                'L' => pos.right(),
+                _ => return None,
             },
-            '-' => match prev {
-                None => Some(pos.right()),
-                Some(_) => Some(Point {
-                    x: pos.x,
-                    y: (pos.y << 1) - prev.unwrap().y,
-                }),
-            },
-            'F' => match prev {
-                None => Some(pos.right()),
-                Some(_) => Some(Point {
-                    x: (pos.x << 1) - prev.unwrap().x + 1, // below => prev.x = pos.x + 1, right => pos.x = prev.x
-                    y: (pos.y << 1) - prev.unwrap().y + 1, // below => prev.y = pos.y, right => pos.y = prev.y + 1
-                }),
-            },
-            '7' => match prev {
-                None => Some(pos.below()),
-                Some(_) => Some(Point {
-                    x: (pos.x << 1) - prev.unwrap().x + 1, // below => prev.x = pos.x + 1, left => pos.x = prev.x
-                    y: (pos.y << 1) - prev.unwrap().y - 1, // below => prev.y = pos.y, left => pos.y = prev.y - 1
-                }),
-            },
-            'J' => match prev {
-                None => Some(pos.above()),
-                Some(_) => Some(Point {
-                    x: (pos.x << 1) - prev.unwrap().x - 1, // above => prev.x = pos.x - 1, left => pos.x = prev.x
-                    y: (pos.y << 1) - prev.unwrap().y - 1, // above => prev.y = pos.y, left => pos.y = prev.y - 1
-                }),
-            },
-            'L' => match prev {
-                None => Some(pos.right()),
-                Some(_) => Some(Point {
-                    x: (pos.x << 1) - prev.unwrap().x - 1, // above => prev.x = pos.x - 1, left => pos.x = prev.x
-                    y: (pos.y << 1) - prev.unwrap().y + 1, // below => prev.y = pos.y, right => pos.y = prev.y + 1
-                }),
-            },
-            _ => None,
         };
-        if next.is_none() || !next.unwrap().is_valid(max_x, max_y) {
+        if !next.is_valid(max_x, max_y) {
             return None;
         }
 
-        result.insert(pos);
-        (prev, pos) = (Some(pos), next.unwrap());
-        pipe = lines[pos.x as usize][pos.y as usize];
-        if pos == start {
+        if pos != start {
+            pipe = &lines[next.x as usize][next.y as usize];
+        } else {
             pipe = treat
         }
+
         // check that we can actually connect by matching pipe
-        if prev.unwrap().x < pos.x {
-            // pos below prev
+        if pos.x < next.x {
             match pipe {
                 '|' | 'L' | 'J' => {}
                 _ => return None,
             }
-        } else if prev.unwrap().x > pos.x {
-            // pos above prev
+        } else if pos.x > next.x {
             match pipe {
                 '|' | 'F' | '7' => {}
                 _ => return None,
             }
-        } else if prev.unwrap().y < pos.y {
-            // pos right to prev
+        } else if pos.y < next.y {
             match pipe {
                 '-' | 'J' | '7' => {}
                 _ => return None,
             }
-        } else if prev.unwrap().y > pos.y {
+        } else if pos.y > next.y {
             // pos left to prev
             match pipe {
                 '-' | 'L' | 'F' => {}
                 _ => return None,
             }
-        } else {
-            return None;
         }
+
+        result.insert(pos);
+        (prev, pos) = (Some(pos), next);
     }
     Some(result)
 }
