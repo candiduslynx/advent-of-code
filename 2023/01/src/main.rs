90f724d82569cc7ff6d2473bd6ccb5b0d5a42261
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-12-11 13:57:25 +0200

    Ref/2023.01.2 (#15)
    * try
    
    * upd test
    
    * use u8

--- 2023/01/src/main.rs
+++ 2023/01/src/main.rs
@@ -5,5 +5,5 @@ mod part2;
 
 fn main() {
     solve_part(part1::solve, "part 1", None);
-    solve_part(part2::solve, "part 2", None);
+    solve_part(part2::solve, "part 2", Some("./test2.txt"));
 }
--- 2023/01/src/part2.rs
+++ 2023/01/src/part2.rs
@@ -7,29 +7,36 @@ pub(crate) fn solve(path: &str) -> u32 {
         .lines()
         .map(|s| s.unwrap())
         .filter(|s| !s.is_empty())
-        .map(|s| digits(s.as_str()))
-        .map(|digits| digits.first().unwrap() * 10 + digits.last().unwrap())
+        .map(|s| num(s.as_str()))
         .sum()
 }
 
-fn digits(s: &str) -> Vec<u32> {
-    s.char_indices()
-        .filter_map(|(idx, _)| digit(&s[idx..]))
-        .collect()
+fn num(s: &str) -> u32 {
+    let mut padded = String::from("____");
+    padded.push_str(s);
+    padded.push_str("____");
+    let first = s
+        .as_bytes()
+        .iter()
+        .enumerate()
+        .find_map(|(i, _)| first_digit(&padded[i + 4..]))
+        .unwrap();
+    let len = padded.len();
+    let last = s
+        .as_bytes()
+        .iter()
+        .enumerate()
+        .find_map(|(i, _)| last_digit(&padded[..len - i - 4]))
+        .unwrap();
+
+    (first * 10 + last) as u32
 }
 
-fn digit(s: &str) -> Option<u32> {
-    if s.is_empty() {
-        return None;
-    }
-
-    match s.chars().next() {
-        None => return None,
-        Some(c) => {
-            if c.is_ascii_digit() {
-                return c.to_digit(10);
-            }
-        }
+fn first_digit(s: &str) -> Option<u8> {
+    let bytes = s.as_bytes();
+    let c = &bytes[0];
+    if c.is_ascii_digit() {
+        return Some(c - b'0');
     }
 
     [
@@ -39,7 +46,28 @@ fn digit(s: &str) -> Option<u32> {
     .enumerate()
     .find_map(|(i, &pfx)| {
         if s.starts_with(pfx) {
-            Some(i as u32)
+            Some(i as u8)
+        } else {
+            None
+        }
+    })
+}
+
+fn last_digit(s: &str) -> Option<u8> {
+    let bytes = s.as_bytes();
+    let c = &bytes[bytes.len() - 1];
+    if c.is_ascii_digit() {
+        return Some(c - b'0');
+    }
+
+    [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .iter()
+    .enumerate()
+    .find_map(|(i, &pfx)| {
+        if s.ends_with(pfx) {
+            Some(i as u8)
         } else {
             None
         }
