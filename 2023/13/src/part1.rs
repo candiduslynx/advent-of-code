d7d6980956faf119e20b9f02263c26df59d640ba
candiduslynx <candiduslynx@gmail.com>
2023-12-13 12:51:40 +0200

    look at data

--- 2023/13/src/part1.rs
+++ 2023/13/src/part1.rs
@@ -2,5 +2,22 @@ use std::fs::read;
 use std::io::BufRead;
 
 pub(crate) fn solve(path: &str) -> u64 {
-    read(path).unwrap().lines().map(|s| s.unwrap()).count() as u64
+    read(path)
+        .unwrap()
+        .lines()
+        .map(|s| s.unwrap())
+        .fold(vec![Vec::<Vec<u8>>::new()], |mut state, s| {
+            match s.len() {
+                0 => state.push(vec![]),
+                _ => state.last_mut().unwrap().push(s.as_bytes().to_vec()),
+            }
+            state
+        })
+        .iter()
+        .for_each(|v| {
+            let x = v.len();
+            let y = v[0].len();
+            println!("{x} by {y}");
+        });
+    124
 }
