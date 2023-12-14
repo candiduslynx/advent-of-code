a2a3972f2f72f71d4b56481ac8fd09ce95b56399
candiduslynx <candiduslynx@gmail.com>
2023-12-14 15:06:42 +0200

    init

---
+++ 2023/14/src/part1.rs
@@ -0,0 +1,11 @@
+use std::fs::read;
+use std::io::BufRead;
+
+pub(crate) fn solve(path: &str) -> u64 {
+    read(path)
+        .unwrap()
+        .lines()
+        .map(|s| s.unwrap())
+        .filter(|s| !s.is_empty())
+        .count() as u64
+}
