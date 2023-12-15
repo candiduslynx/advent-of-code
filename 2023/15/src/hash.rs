b4ee545813d62633b44a38618226521616721bb8
candiduslynx <candiduslynx@gmail.com>
2023-12-15 18:35:04 +0200

    15.2

--- 2023/15/src/hash.rs
+++ 2023/15/src/hash.rs
@@ -1,9 +1,9 @@
-pub(crate) fn hash(s: &str) -> u8 {
-    s.as_bytes().iter().fold(0u16, |s, &b| {
-        const U8_MASK: u16 = 255;
-        let s = (s + b as u16) & U8_MASK;
+pub(crate) fn hash(s: &str) -> usize {
+    s.as_bytes().iter().fold(0usize, |s, &b| {
+        const U8_MASK: usize = 255;
+        let s = (s + b as usize) & U8_MASK;
         let p = (s << 4) + s;
         let n = p & U8_MASK;
         n
-    }) as u8
+    })
 }
--- 2023/15/src/part2.rs
+++ 2023/15/src/part2.rs
@@ -4,11 +4,55 @@ use std::io::BufRead;
 use crate::hash;
 
 pub(crate) fn solve(path: &str) -> u64 {
+    type KeyValue = (String, usize);
+    let mut boxes: Vec<Vec<Option<KeyValue>>> = std::iter::repeat(vec![])
+        .take(256)
+        .collect::<Vec<Vec<Option<KeyValue>>>>();
+
     read(path)
         .unwrap()
         .lines()
         .map(|s| s.unwrap())
         .filter(|s| !s.is_empty())
-        .map(|s| s.split(",").map(|s| hash::hash(s) as u64).sum::<u64>())
+        .for_each(|s| {
+            s.split(",").for_each(|part| {
+                if part.ends_with("-") {
+                    let key = part.strip_suffix("-").unwrap();
+                    match boxes[hash::hash(key)]
+                        .iter_mut()
+                        .find(|s| s.as_ref().is_some_and(|(s, _)| s == key))
+                    {
+                        None => {}
+                        Some(k) => *k = None,
+                    }
+                } else {
+                    let parts: Vec<&str> = part.split("=").collect();
+                    assert_eq!(parts.len(), 2);
+                    let key = parts[0];
+                    let val = parts[1].parse::<usize>().unwrap();
+                    match boxes[hash::hash(key)]
+                        .iter_mut()
+                        .find(|s| s.as_ref().is_some_and(|(s, _)| s == key))
+                    {
+                        None => boxes[hash::hash(key)].push(Some((String::from(key), val))),
+                        Some(Some(k)) => k.1 = val,
+                        _ => {}
+                    }
+                }
+            })
+        });
+
+    boxes
+        .iter()
+        .enumerate()
+        .filter(|(_, v)| !v.is_empty())
+        .map(|(i, v)| {
+            let k = i + 1;
+            v.iter()
+                .filter(|s| s.is_some())
+                .enumerate()
+                .map(|(j, kv)| (k * (j + 1) * kv.as_ref().unwrap().1) as u64)
+                .sum::<u64>()
+        })
         .sum()
 }
