1de27bf1eb32c6edadb4c260693a124d9d6b51a9
candiduslynx <candiduslynx@gmail.com>
2023-12-16 15:39:42 +0200

    small ref

--- 2023/15/src/part2.rs
+++ 2023/15/src/part2.rs
@@ -1,6 +1,7 @@
-use crate::lens::LensHasher;
 use std::fs::read;
 use std::io::BufRead;
+
+use crate::lens::LensHasher;
 
 pub(crate) fn solve(path: &str) -> u64 {
     let mut boxes = std::iter::repeat(vec![])
@@ -13,9 +14,9 @@ pub(crate) fn solve(path: &str) -> u64 {
         .map(|s| s.unwrap())
         .filter(|s| !s.is_empty())
         .for_each(|s| {
-            s.split(",").for_each(|part| {
-                if part.ends_with("-") {
-                    let key = part.strip_suffix("-").unwrap();
+            s.split(",").for_each(|part| match part.as_bytes().last() {
+                Some(&b'-') => {
+                    let key = &part[..part.len() - 1];
                     match boxes[LensHasher::calc(key) as usize]
                         .iter_mut()
                         .find(|s| s.as_ref().is_some_and(|(s, _)| s == key))
@@ -23,17 +24,18 @@ pub(crate) fn solve(path: &str) -> u64 {
                         None => {}
                         Some(k) => *k = None,
                     }
-                } else {
+                }
+                _ => {
                     let parts: Vec<&str> = part.split("=").collect();
                     assert_eq!(parts.len(), 2);
                     let key = parts[0];
-                    let h = usize::try_from(LensHasher::calc(key)).unwrap();
+                    let lens_box = &mut boxes[LensHasher::calc(key) as usize];
                     let val = parts[1].parse::<u8>().unwrap();
-                    match boxes[h]
+                    match lens_box
                         .iter_mut()
                         .find(|s| s.as_ref().is_some_and(|(s, _)| s == key))
                     {
-                        None => boxes[h].push(Some((String::from(key), val))),
+                        None => lens_box.push(Some((String::from(key), val))),
                         Some(Some(k)) => k.1 = val,
                         _ => {}
                     }
